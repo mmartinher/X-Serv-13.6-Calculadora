5be0e8674faafb10a55d48ccd79836957deebdfc
Marina Martin Hernandez <mmartin@zeta40.aulas.gsyc.es>
2016-02-11 11:00:12 +0100

    Ejercicio 13.6

--- calculadora.py
+++ calculadora.py
@@ -1,36 +1,40 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Marina Martín Hernández
+# Ejercicio 13.5:
 
 import sys
 
 correct = False
 
 if len (sys.argv) == 4:
-    correct = True
-    operator = sys.argv[1]
-    number1 = sys.argv[2]
-    number2 = sys.argv[3]
-
-def calculator (num1, num2, operator):
-    if operator == 'add':   #ADDITION
-        return int(num1) + int(num2)
-    elif operator == 'sub': #SUBTRACTION
-        return int(num1) - int(num2)
-    elif operator == 'mult': #MULTIPLICATION   
-        return int(num1) * int(num2)
-    elif operator == 'div': #DIVISION
-        try:
-            return int(num1) / int(num2)
-        except ZeroDivisionError:
-            print "Error. You're trying to divide by 0."
-    else:
-        return "Error. Insert the correct operator: sum, sub, mult, div."
+	correct = True
+	function = sys.argv[1]
+	element1 = sys.argv[2]
+	element2 = sys.argv[3]
+	
+def calculator (function, element1, element2):
+	if function == 'add':
+		return int(element1) + int(element2)
+	elif function == 'sub':
+		return int(element1) - int(element2)
+	elif function == 'mult':
+		return int(element1) * int(element2)
+	elif function == 'div':
+		try:
+			return int(element1) / int(element2)
+		except ZeroDivisionError:
+			print("ERROR. You're trying to divide by 0.\n")
+	else:
+		return "ERROR. Insert the correct operator: add, sub, mult, div.\n"
 
 
 if __name__ == "__main__":
-    if correct:
-        try:
-            print (calculator(number1, number2, operator))
-        except ValueError:
-            print "Error. Insert numbers for operate."
-    else:
-        print "Error. Insert the correct parameter."
+	if correct:
+		try:
+			print (calculator(function, element1, element2))
+		except ValueError:
+			print ("Error. Insert numbers for operate.")
+	else:
+		print ("Error. Insert the correct parameter.")
